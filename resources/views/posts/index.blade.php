df135e486bcb844f815237e8adc9e55057dbda0d
Odette Simons <odette.simons@live.com>
2021-07-09 14:19:36 -0400

    feature(WIP: New laravel project): testing new development
    testing new development
    
    BREAKING CHANGE: none
    
    Closes none

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -32,6 +32,14 @@
                         <a href="" class="font-bold">{{ $post->user->name }}</a>
                         <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
+                        <div class="flex items-center">
+                            <form action="" method="post" class="mr-1">
+                                <button type="submit" class="text-blue-500">Like</button>
+                            </form>
+                            <form action="" method="post" class="mr-1">
+                                <button type="submit" class="text-blue-500">Unlike</button>
+                            </form>
+                        </div>
                     </div>
                 @endforeach
                 {{--pagination footer--}}
